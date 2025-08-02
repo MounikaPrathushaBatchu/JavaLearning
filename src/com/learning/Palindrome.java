2c022073b418e9f29c764d86392f9e77c45456b5
Mounika Batchu <Mounika.Batchu@syenainfosoft.com>
2025-08-02 12:33:30 +0530

    palindrome and string reverse programs

---
+++ src/com/learning/Palindrome.java
@@ -0,0 +1,33 @@
+package com.learning;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String input = "Abcddcba";
+		boolean palindrome = isPalindrom(input);
+		System.out.println("given String :"+input);
+		
+		if(palindrome){
+			System.out.println(input+" is palindrome");
+		} else{
+			System.out.println(input+" is not palindrome");
+		}
+
+	}
+	
+	public static boolean isPalindrom(String str) {
+		
+		String result = "";
+		boolean palindrome = false;
+		for(int i = str.length() - 1; i >= 0; i--){
+			result += str.charAt(i);
+		}
+		
+		if(str.equalsIgnoreCase(result)){
+			palindrome = true;
+		}
+		
+		return palindrome;
+	}
+
+}
